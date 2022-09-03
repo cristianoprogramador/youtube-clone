d0709c3bb35826915da73d7c144d026016697d5a
Cristiano Silva <cristiano_own@hotmail.com.br>
2022-09-03 18:36:44 -0300

    Site finalizado com exibição de video

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import { useParams } from 'react-router-dom'
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { Videos } from './'
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, [searchTerm])
+
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }} >
+      Resultados da busca por: <span style={{ color: '#f31503' }}>{searchTerm}</span> vídeos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
